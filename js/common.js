a2dfa22600c6d052d40e043a962c0954d4e33f25
jjang051 <flyjjang051@gmail.com>
2022-11-07 11:37:12 +0900

    header end

--- js/common.js
+++ js/common.js
@@ -20,12 +20,53 @@ btnAll.on("click", function (e) {
   e.preventDefault();
   const icon = $(this).find(".material-icons");
   //console.log(icon.text());
-  if (icon.text() === "menu") {
-    icon.text("close");
-    console.log("close");
-  } else {
+  // if (icon.text() === "menu") {
+  //   icon.text("close");
+  //   console.log("close");
+  // } else {
+  //   icon.text("menu");
+  //   console.log("menu");
+  // }
+  if (gnb.hasClass("on")) {
     icon.text("menu");
-    console.log("menu");
+  } else {
+    icon.text("close");
   }
   gnb.toggleClass("on");
 });
+
+//js에서 크기를 제어해야한다.
+
+$(window).on("resize", function () {
+  console.log($(window).width());
+  const w = $(window).outerWidth();
+  console.log(w);
+  if (w > 1280) {
+    $("html").removeClass("m").addClass("pc");
+    $("#gnb .depth02").removeAttr("style");
+    $("#gnb").removeClass("on");
+    const icon = $(".btn-all .material-icons");
+    icon.text("menu");
+    // gnb에 on을 제거하고 btn-all안에 있는 material-icons의 텍스트 menu로 바꾸기...
+  } else {
+    $("html").removeClass("pc").addClass("m");
+  }
+});
+
+const depth01 = $("#gnb .depth01");
+depth01.on("click", function (e) {
+  if ($("html").hasClass("m")) {
+    const depth02 = $(this).next();
+    const siblings = $(this).parent().siblings().find(".depth02");
+    const siblingsDepth01 = $(this).parent().siblings().find(".depth01");
+    $(this).toggleClass("on");
+    siblingsDepth01.removeClass("on");
+    siblings.slideUp();
+    if (depth02.length > 0) {
+      e.preventDefault();
+      depth02.stop().slideToggle();
+    }
+  }
+});
+
+$(window).trigger("resize");
