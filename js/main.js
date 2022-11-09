18f19142609309a1c8466771d0e92de243ac8e28
jjang051 <flyjjang051@gmail.com>
2022-11-09 15:19:26 +0900

    footer

--- js/main.js
+++ js/main.js
@@ -35,8 +35,8 @@ const introduceSlider = new Swiper("#introduce .banner", {
   },
 });
 const activitySlider = new Swiper("#activity  .banner", {
-  slidesPerView: 4,
-  slidesPerGroup: 4,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
   loop: true,
   spaceBetween: 20,
 
@@ -45,10 +45,25 @@ const activitySlider = new Swiper("#activity  .banner", {
     nextEl: "#activity .btn-next",
     prevEl: "#activity .btn-prev",
   },
+  breakpoints: {
+    // when window width is >= 320px
+    640: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+    1400: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+    },
+  },
 });
 const noticeSlider = new Swiper("#notice  .banner", {
-  slidesPerView: 3,
-  slidesPerGroup: 3,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
   loop: true,
   spaceBetween: 20,
 
@@ -57,4 +72,20 @@ const noticeSlider = new Swiper("#notice  .banner", {
     nextEl: "#notice .btn-next",
     prevEl: "#notice .btn-prev",
   },
+  breakpoints: {
+    // when window width is >= 320px
+    640: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+  },
+});
+const familySlider = new Swiper("footer #family-site .container", {
+  spaceBetween: 30,
+  slidesPerView: "auto",
+  freeMode: true,
 });
